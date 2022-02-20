fa54e89795c6fcbd526e732862d1949df20ff7aa
karthedew <karthedew@gmail.com>
2022-02-19 16:42:12 -0800

    AtlanticId version 1.0_beta

--- truffle/contracts/AtlanticId.sol
+++ truffle/contracts/AtlanticId.sol
@@ -12,8 +12,8 @@ import "./DateTime.sol";
 
 /**
 TODO: Add EVENTS
-TODO: Change Transfer functions
-TODO: Add AtlanticId Functions
+        EVENT 1] Mint(tokenId)
+        EVENT 2]
  */
 
 contract AtlanticId is
@@ -43,14 +43,21 @@ contract AtlanticId is
         string ens;                               //> | User's name
         string uid;                               //> | User's Identification Number
         string exchange;                          //> | Centralized Exchange where user's KYC info is registered
+        uint256 tokenId;                          //> | User's NFT token ID.
         DateTime._DateTime expiry;                //> | Expiration date - period of valid ID
     }
     mapping(uint256 => AID) id_to_AID;          // TokenId mapped to User's Information
     mapping(address => string) approved_key;    // Checks if address has an approved key.
+    mapping(address => bool) transfer_key;      // Checks if address has an approved key.
+    mapping(string => uint) _months;            // Mapping of months to numbers
+
+    /** EVENTS */
+    event Mint(uint256 tokenId, uint timestamp, uint256 yearExpired);
+
 
     /** CONSTRUCTOR */
     constructor() ERC721("AtlanticId","AID") {
-        atlanticIdOwner = msg.sender;
+        atlanticIdOwner = _msgSender();
         // --- SET ROLES ---
         _setupRole(MINTER_ROLE, atlanticIdOwner);
         _setupRole(PAUSER_ROLE, atlanticIdOwner);
@@ -116,13 +123,22 @@ contract AtlanticId is
         address from,
         address to,
         uint256 tokenId
-    ) public override
-      
+    ) public override canTransfer(_msgSender())
     {
-        
+        _transfer(from, to, tokenId);
+        delete transfer_key[_msgSender()];
     }
 
-
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public override canTransfer(_msgSender())
+    {
+        _safeTransfer(from, to, tokenId, _data);
+        delete transfer_key[_msgSender()];
+    }
 
     // ==============================
     /** ATLANTICID SPECIFIC FUNCTIONS */
@@ -158,15 +174,18 @@ contract AtlanticId is
         string memory _exchange,
         string memory _key
     ) public
-      hasKey(msg.sender, _key)
+      hasKey(_msgSender(), _key)
     {
+        /** FUNCTION VARIABLES */
+        uint _timestamp = block.timestamp;
         // STEP 1: Require MINTER_ROLE & _key
         require(hasRole(MINTER_ROLE, _msgSender()), "ERC721PresetMinterPauserAutoId: must have minter role to mint");
-        
         uint256 _tokenId = _tokenIdTracker.current();
+
         // STEP 2: Calculate the Expiration Date
         // --- Calculate the Expiration Date ---
-        DateTime._DateTime memory _expiry = DateTime.parseTimestamp(block.timestamp);
+        DateTime._DateTime memory _expiry = DateTime.parseTimestamp(_timestamp);
+        _expiry.year = _expiry.year + 1;
 
         // STEP 3: Create Mapping
         // --- Map the NFT IdNumber to the Data --- 
@@ -174,18 +193,20 @@ contract AtlanticId is
             ens: _ens,
             uid: _uid,
             exchange: _exchange,
-            expiry: _expiry
+            expiry: _expiry,
+            tokenId: _tokenId
         });
 
         // STEP 4: Mint NFID
-        _safeMint(msg.sender, _tokenId);
+        _safeMint(_msgSender(), _tokenId);
 
         // SETUP 5: Allow atlanticIdOwner to Manage NFT
         approve(atlanticIdOwner, _tokenId);
+        emit Mint(_tokenId, _timestamp, _expiry.year);
         _tokenIdTracker.increment();
 
         // STEP 6: Delete _key
-        delete approved_key[msg.sender];
+        delete approved_key[_msgSender()];
     }
 
 
@@ -196,23 +217,84 @@ contract AtlanticId is
         TODO: Add isValidAid()  - This function could use Chainlink (recommended) to bring in the current date, 
                                   or the function could take in what the current date it is (not recommended).
         TODO: Add getUid()      - Returns AID user ID.
+        TODO: Add getEns()
+        TODO: Add getExchange()
+        TODO: Add getExpry()
      */
 
-    function getExpiry(
+    function getUid(
+        uint256 _tokenId
+    ) public view
+      returns (string memory)
+    {
+        require(_exists(_tokenId),"ERC721: tokenId does not exist.");
+        return id_to_AID[_tokenId].uid;
+    }
+
+    function getEns(
+        uint256 _tokenId
+    ) public view 
+      returns (string memory)
+    {
+        require(_exists(_tokenId),"ERC721: tokenId does not exist.");
+        require(keccak256(abi.encodePacked(_msgSender())) == keccak256(abi.encodePacked(atlanticIdOwner)),"ERC721: ");
+        return id_to_AID[_tokenId].ens;
+    }
+
+    function getExchange(
+        uint256 _tokenId
+    ) public view
+      returns (string memory)
+    {
+        require(_exists(_tokenId),"ERC721: tokenId does not exist.");
+        return id_to_AID[_tokenId].exchange;
+    }
+
+    function getExpry(
         uint256 _tokenId
     ) public view
       returns (uint8 day, uint8 month, uint16 year)
     {
+        require(_exists(_tokenId),"ERC721: tokenId does not exist.");
         AID memory user_aid = id_to_AID[_tokenId];
         DateTime._DateTime memory user_date = user_aid.expiry;
-
         return (user_date.day, user_date.month, user_date.year);
     }
 
-    function isValidAid() public view {
+    function isValidAid(
+        uint256 _tokenId
+    ) public view
+      returns (bool)
+    {
+        require(_exists(_tokenId),"ERC721: tokenId does not exist.");
+        uint16 now_year = DateTime.getYear(block.timestamp);
+        uint8 now_month = DateTime.getMonth(block.timestamp);
+        uint8 now_day = DateTime.getDay(block.timestamp);
 
+        AID memory user_aid = id_to_AID[_tokenId];
+        DateTime._DateTime memory user_date = user_aid.expiry;
+        uint16 id_year = user_date.year;
+        uint8 id_month = user_date.month;
+        uint8 id_day = user_date.day;
+
+        if (id_year < now_year) {
+            return true;
+        } else if (id_year > now_year) {
+            return false;
+        } else {
+            if (now_month < id_month) {
+                return true;
+            } else if (now_month > id_month) {
+                return false;
+            } else {
+                if (now_day < id_day) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
     }
-
 
     /** MODIFIERS */
     // keccak256(abi.encodePacked(a)
@@ -220,6 +302,12 @@ contract AtlanticId is
       if (keccak256(abi.encodePacked(approved_key[_sender])) == keccak256(abi.encodePacked(_key))) {
          _;
       }
-   }
+    }
+
+    modifier canTransfer(address _sender) {
+      if (transfer_key[_sender]) {
+         _;
+      }
+    }
 
 }
